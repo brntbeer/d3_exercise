b7d7424f5d86c9732a43b8a2e0c961b33b9f894a
brntbeer <brent.beer@gmail.com>
2012-07-27 01:54:53 +0200

    Somehow forgot to add all of this when changing to use rackup

--- d3.rb
+++ d3.rb
@@ -1,32 +1,37 @@
-require 'rubygems'
-require 'sinatra'
+require 'rack-flash'
+require "pry"
+class D3Example < Sinatra::Base
+  enable :sessions
+  use Rack::Flash, :sweep => true,
+                   :accessorize => [:notice, :warning, :error]
 
+  error 400..510 do
+    flash[:warning] = "Sorry, that page either doesn't exist or is down"
+    redirect "/bargraph"
+  end
 
-get '/' do
-  @title = "d3.js exercise BEGIN!"
-  erb :index
-end
+  get '/' do
+    redirect '/bargraph'
+  end
 
-get '/bargraph' do
-  erb :bargraph
-end
+  get '/bargraph' do
+    erb :bargraph
+  end
 
-get '/bargraph2' do 
-  erb :bargraph2
-end
+  get '/bargraph2' do 
+    erb :bargraph2
+  end
 
-get '/bouncybawls' do
-  erb :bouncybawls
-end
+  get '/bouncybawls' do
+    erb :bouncybawls
+  end
 
-get '/population' do
-  erb :population
-end
+  get '/population' do
+    erb :population
+  end
 
-get "/bar" do
-  erb :bar
-end
+  get "/ball" do
+    erb :ball
+  end
 
-get "/ball" do
-  erb :ball
 end
