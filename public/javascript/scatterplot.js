04c9e0b77cb22af90faeb370bdc867f0ee9b6a4e
brntbeer <brent.beer@gmail.com>
2012-07-27 20:10:20 +0200

    added additional datapoint changed graph height, scaled for r

--- public/javascript/scatterplot.js
+++ public/javascript/scatterplot.js
@@ -1,17 +1,23 @@
 var dataset = [ [5,20], [480, 90], [250,50], [100,33], [330, 95],
-                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+                [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
+                [610, 145]
               ];
 
 var w = 500;
-var h = 100;
+var h = 100+(5*dataset.length);
+var padding = 20;
 
 var xScale = d3.scale.linear()
                      .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-                     .range([0, w])
+                     .range([padding, w-padding*2])
 
 var yScale = d3.scale.linear()
                      .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-                     .range([h, 0]);
+                     .range([h - padding, padding]);
+
+var rScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset, function(d) {return d[0]+d[1];})])
+                     .range([2,8])
 
 var svg = d3.select("body")
             .append("svg")
@@ -24,7 +30,7 @@ svg.selectAll("circles")
    .append("svg:circle")
    .attr("cx", function(d) { return xScale(d[0]); })
    .attr("cy", function(d) { return yScale(d[1]); })
-   .attr("r", function(d) { return Math.sqrt(d[0] + d[1]);});
+   .attr("r", function(d) { return rScale(d[0]+d[1]); });
 
 svg.selectAll("text")
    .data(dataset)
