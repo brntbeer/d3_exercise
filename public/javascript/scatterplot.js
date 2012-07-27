b26d49c3f75bf84c20f0eb16bfae7109dea6db0c
brntbeer <brent.beer@gmail.com>
2012-07-27 19:59:24 +0200

    added scales and changed position of circles

--- public/javascript/scatterplot.js
+++ public/javascript/scatterplot.js
@@ -4,6 +4,15 @@ var dataset = [ [5,20], [480, 90], [250,50], [100,33], [330, 95],
 
 var w = 500;
 var h = 100;
+
+var xScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+                     .range([0, w])
+
+var yScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                     .range([h, 0]);
+
 var svg = d3.select("body")
             .append("svg")
             .attr("width", w)
@@ -13,16 +22,16 @@ svg.selectAll("circles")
    .data(dataset)
    .enter()
    .append("svg:circle")
-   .attr("cx", function(d) { return d[0]; })
-   .attr("cy", function(d) { return d[1]; })
+   .attr("cx", function(d) { return xScale(d[0]); })
+   .attr("cy", function(d) { return yScale(d[1]); })
    .attr("r", function(d) { return Math.sqrt(h - d[1]);});
 
 svg.selectAll("text")
    .data(dataset)
    .enter()
    .append("text")
-   .attr("x", function(d) { return d[0]; })
-   .attr("y", function(d) { return d[1]; })
+   .attr("x", function(d) { return xScale(d[0]); })
+   .attr("y", function(d) { return yScale(d[1]); })
    .attr("font-family", "sans-serif")
    .attr("font-size", "11px")
    .attr("fill", "red")
